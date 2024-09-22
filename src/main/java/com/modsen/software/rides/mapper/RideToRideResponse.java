0c0ea0673e2f71b9a7c49000633df2437ddec743
aleksej <man747704@gmail.com>
2024-09-23 01:33:09 +0300

    feat(repository): implement repository with migrations

---
+++ src/main/java/com/modsen/software/rides/mapper/RideToRideResponse.java
@@ -0,0 +1,25 @@
+package com.modsen.software.rides.mapper;
+
+import com.modsen.software.rides.dto.RideResponse;
+import com.modsen.software.rides.entity.Ride;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RideToRideResponse implements Converter<Ride, RideResponse> {
+
+    @Override
+    public RideResponse convert(Ride source) {
+        return RideResponse.builder()
+                .id(source.getId())
+                .price(source.getPrice())
+                .completionDate(source.getCompletionDate())
+                .creationDate(source.getCreationDate())
+                .departureAddress(source.getDepartureAddress())
+                .rideStatus(source.getRideStatus().toString())
+                .destinationAddress(source.getDestinationAddress())
+                .driverId(source.getDriverId())
+                .passengerId(source.getPassengerId())
+                .build();
+    }
+}
