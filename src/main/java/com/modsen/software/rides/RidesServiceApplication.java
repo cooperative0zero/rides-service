c6c3ff73deeb7f2ddc22f34b62101e2c45e33b6d
aleksej <man747704@gmail.com>
2024-09-22 23:32:13 +0300

    chore: project initialization

---
+++ src/main/java/com/modsen/software/rides/RidesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.modsen.software.rides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidesServiceApplication.class, args);
+	}
+
+}
