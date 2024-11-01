e42a9105eab476eefbb6b3d61540560dd3f83146
aleksej <man747704@gmail.com>
2024-11-01 05:06:41 +0300

    feat: add REST API description with OpenAPI specification(Swagger)

--- src/main/java/com/modsen/software/rides/dto/PaginatedResponse.java
+++ src/main/java/com/modsen/software/rides/dto/PaginatedResponse.java
@@ -1,5 +1,6 @@
 package com.modsen.software.rides.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,17 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Paginated response")
 public class PaginatedResponse<T> {
+    @Schema(description = "Items of the selected page")
     private List<T> items;
+
+    @Schema(description = "Requested page number")
     private Integer page;
+
+    @Schema(description = "Requested page size")
     private Integer size;
+
+    @Schema(description = "Total number of items")
     private Integer total;
 }
